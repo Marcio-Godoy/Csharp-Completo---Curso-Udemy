a295bfaae806decebd21e847376ff99c7124c1f0
Marcio Richard <33956393+MarcioRichard@users.noreply.github.com>
2020-01-27 23:21:56 -0300

    Add files via upload

---
+++ 02_Tipos_de_dados_basicos/02_Tipos_de_dados_basicos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _02_Tipos_de_dados_basicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // CRIANDO VARIAVEIS //
+             
+            bool Completo = false; //Verdadeiro ou Falso (Boolean)
+            char Genero = '\u0041'; //Genero UNICODE por código
+            byte n1 = 255; //Valor máximo suportado
+            sbyte x = 127; //Valor máximo suportado
+            int n2 = 126;
+            int n3 = 2147483647;
+            long n4 = 2147483648L; //Recomendado ao usar long colocar a letra L no final (Recomendação de boas práticas)
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            string Nome = "Marcio";
+            object obj1 = "Marcio Godoy"; //Object: Tipo de dados GENERICO
+            object obj2 = 4.5;
+
+            // FIM DA CRIAÇÃO DE VARIAVEIS //
+
+            Console.WriteLine(Completo);
+            Console.WriteLine(Genero);
+            Console.WriteLine("Tipo sbyte: " + x);
+            Console.WriteLine("Tipo byte: " + n1);
+            Console.WriteLine("Tipo int: " + n2);
+            Console.WriteLine("Tipo int: " + n3);
+            Console.WriteLine("Tipo long: " + n4);
+            Console.WriteLine("Tipo float: " + n5);
+            Console.WriteLine("Tipo double: " + n6);
+            Console.WriteLine("Tipo String: " + Nome);
+            Console.WriteLine("Tipo object: " + obj1);
+            Console.WriteLine("Tipo object: " + obj2);
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ 03_Saida_de_Dados/03_Saida_de_Dados/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace _03_Saida_de_Dados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DECLARANDO VARIÁVEIS
+            char genero = 'F';
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+            //FIM DA DECLARAÇÃO DE VARIÁVEIS
+
+            Console.WriteLine("Bom Dia!");
+            Console.WriteLine("Boa Tarde!");
+            Console.WriteLine("Boa Noite!");
+            Console.WriteLine("Qual é o genero: " + genero);
+            Console.WriteLine("Qual é a idade: " + idade);
+            Console.WriteLine("Qual é o saldo: " + saldo);
+            Console.WriteLine("Qual é o nome: " + nome);
+            Console.WriteLine("Qual é o saldo com duas casas decimais: " + saldo.ToString("F2")); //Delimitando o número de casas decimais
+            Console.WriteLine("Qual é o saldo com quatro casas decimais: " + saldo.ToString("F4"));
+            Console.WriteLine("Qual é o saldo com separação de digitos por ponto: " + saldo.ToString("F4", CultureInfo.InvariantCulture)); //CultureInfo.InvariantCulture serve para definir o valor com o ponto de decimais
+            Console.WriteLine();
+
+            // SAÍDA DE DADOS USANDO PLACEHOLDERS
+            Console.WriteLine("SAÍDA DE DADOS COM PLACEHOLDERS");
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo); //PlaceHolders
+            Console.WriteLine();
+
+            //SAÍDA DE DADOS USANDO INTERPOLAÇÃO
+            Console.WriteLine("SAÍDA DE DADOS COM INTERPOLAÇÃO");
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais"); //Interpolação
+            Console.WriteLine();
+
+            //SAÍDA DE DADOS USANDO CONCATENAÇÃO
+            Console.WriteLine("SAÍDA DE DADOS COM CONCATENAÇÃO"); //Concatenação
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2") + " reais ");
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais ");
+
+           
+
+
+        }
+    }
+}
