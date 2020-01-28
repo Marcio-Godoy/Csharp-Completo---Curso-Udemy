41f1184b8c497ae7d29eccdd517aed6700daac00
Marcio Richard <33956393+MarcioRichard@users.noreply.github.com>
2020-01-27 23:17:46 -0300

    Add files via upload

---
+++ 01_Primeiro/01_Primeiro/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01_Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Identação automática : CTRL + K + D
+            Console.WriteLine("Olá Mundo!");
+        }
+    }
+}
