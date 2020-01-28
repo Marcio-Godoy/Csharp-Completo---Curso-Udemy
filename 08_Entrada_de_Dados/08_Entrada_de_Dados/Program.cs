f1592c76a51b86fc69e580440b3157b5b853a650
Marcio Richard <33956393+MarcioRichard@users.noreply.github.com>
2020-01-27 23:32:25 -0300

    Add files via upload

---
+++ 08_Entrada_de_Dados/08_Entrada_de_Dados/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _08_Entrada_de_Dados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+
+            //Split função para recortar a string
+            string[] v = Console.ReadLine().Split(' '); //Pega cada caractere separado por espaço em branco
+            string a = v[0]; //Vetor
+            string b = v[1]; //Vetor
+            string c = v[2]; //Vetor
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x); 
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            //Split função para recortar a string
+            //string s = Console.ReadLine();
+            //string[] vet = s.Split(' '); //Pega cada caractere separado por espaço em branco
+            //string p1 = vet[0]; //Vetor
+            //string p2 = vet[1]; //Vetor
+            //string p3 = vet[2]; //Vetor
+
+        }
+    }
+}
---
+++ 09_ExPraticos/09_ExPraticos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace _09_ExPraticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa: ");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto: ");
+            double produto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com o seu último nome, idade e altura (mesma linha): ");
+
+            string[] vet = Console.ReadLine().Split(' '); //Criando o vetor vazio e usando o SPLIT para quebrar os caractere
+            string ultimonome = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2]);
+
+            Console.WriteLine("Os dados digitados foram:");
+            Console.WriteLine();
+            Console.WriteLine(nome);
+            Console.WriteLine(quartos);
+            Console.WriteLine(produto.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine(ultimonome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2",CultureInfo.InvariantCulture));
+
+        }
+    }
+}
