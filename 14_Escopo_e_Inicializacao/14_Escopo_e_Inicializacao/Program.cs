373ed40e2b5fe9629056a276856c7c4c6c7dca48
Marcio Richard <33956393+MarcioRichard@users.noreply.github.com>
2020-01-27 23:36:43 -0300

    Add files via upload

---
+++ 14_Escopo_e_Inicializacao/14_Escopo_e_Inicializacao/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _14_Escopo_e_Inicializacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite o valor do preço do produto:");
+
+            double preco = double.Parse(Console.ReadLine());
+            double desconto = 0;
+
+            if(preco > 100.00) {
+                desconto = preco * 0.1;
+            }
+
+            Console.WriteLine(desconto);
+        }
+    }
+}
