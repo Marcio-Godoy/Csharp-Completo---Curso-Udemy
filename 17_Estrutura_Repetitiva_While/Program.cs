85f3d354a12b5f5a490dd09ac7c57265e25b7d29
Marcio Richard <33956393+MarcioRichard@users.noreply.github.com>
2020-01-28 11:24:38 -0300

    Add files via upload

---
+++ 17_Estrutura_Repetitiva_While/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace _17_Estrutura_Repetitiva_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (x >= 0.0)
+            {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+                Console.WriteLine("Digire outro número: ");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            Console.WriteLine("Número negativo!, Fim da execução do programa!");
+        }
+    }
+}
